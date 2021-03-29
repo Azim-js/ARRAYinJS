b40e6a72761db68e5025ca99a145a11d33fad6b0
Azim <66790056+Azim-js@users.noreply.github.com>
2021-03-29 23:26:51 +0530

    using .entries() .has() .add() .delete() to sets

--- setsmaps.js
+++ setsmaps.js
@@ -7,3 +7,24 @@ const ids=new Set([1,2,3,4,5]);
 ids.add(2); //NO DUBLICATION
 
 console.log(ids);
+
+console.log(ids.has(1)); //for checking
+
+const ids1= new Set(["HI","From","HEllo"]);
+
+ids1.add(2)
+
+console.log(ids1);
+
+// for iterating in sets we use set.entries() method
+
+for(const entry of ids1.entries()){
+    console.log(entry);
+}
+
+// to remove elements from sets we use .delete()
+
+if(ids1.has("HI")){
+    ids1.delete("HI");
+}
+console.log(ids1);
